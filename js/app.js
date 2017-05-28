c164cf50d2dd8d42b763524d57038ab0e2830f41
FishingOnATree <raysung78@gmail.com>
2017-05-28 10:12:58 -0400

    Created all necessary methods/inheritance so the board shows.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 // Enemies our player must avoid
+var Char = function(imageURL) {
+    this.sprite = imageURL;
+};
+
+Char.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    Char.call(this,'images/enemy-bug.png');
 };
+
+Enemy.prototype = Object.create(Char.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,20 +27,29 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    Char.call(this,'images/char-boy.png');
+};
 
-
+Player.prototype = Object.create(Char.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+Player.prototype.handleInput = function(keycode) {
+    console.log(keycode);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+allEnemies.push(new Enemy());
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
